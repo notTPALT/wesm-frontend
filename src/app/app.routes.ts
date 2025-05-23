d5752495447d44c87f1fe52a02ecd683632c8860
Phu Bui <tphualt@gmail.com>
2025-05-23 18:49:02 +0700

    feat: add route to details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { 
+    path: '', 
+    component: DashboardComponent,
+    title: 'WESM Dashboard' 
+  },
+  {
+    path: 'details',
+    component: DetailsComponent,
+    title: 'What the details'
+  },
 ];
